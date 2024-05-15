7463d10dbbcf3183ee76bc3b8f4d603ffce7d266
ItsTheKGV <keanugene.valibia@gmail.com>
2024-05-15 01:32:08 -0500

    Added menu.csv and order.csv. Edited Python Analysis.py file.

--- Python Analysis.py
+++ Python Analysis.py
@@ -1,3 +1,15 @@
 import pandas as pd
 
-detail = pd.read_csv('detail.csv')
+## import csv files as dataframes
+detail = pd.read_csv('Raw Data/detail.csv')
+menu = pd.read_csv('Raw Data/menu.csv')
+order = pd.read_csv('Raw Data/order.csv')
+
+# preview first five rows of each df
+print(detail.head(5))
+print(menu.head(5))
+print(order.head(5))
+
+print(detail.dtypes)
+print(menu.dtypes)
+print(order.dtypes)
