d755e87b0afb0696b1acf05cad2cd92e66ed6b5c
ItsTheKGV <keanugene.valibia@gmail.com>
2024-06-03 01:51:39 -0500

    Changes

--- Python Analysis.py
+++ Python Analysis.py
@@ -1,13 +1,16 @@
+# %%
 import pandas as pd
 import numpy as np
 import matplotlib as mp
 import seaborn as sb
 
+# %%
 ## import csv files as dataframes
 detail = pd.read_csv('Raw Data/detail.csv')
 menu = pd.read_csv('Raw Data/menu.csv')
 order = pd.read_csv('Raw Data/order.csv')
 
+# %%
 # preview first five rows of each df
 print(detail.head(5))
 print(menu.head(5))
@@ -22,3 +25,6 @@ detailed_orders = detail.merge(menu, how = "inner", on = "product_id")
 detailed_orders = detailed_orders.drop(columns=["price_x"])
 detailed_orders = detailed_orders.rename(columns={"price_y":"price"})
 print(detailed_orders.head(10))
+
+# %%
+sb.lineplot(order, x = "date", y = "total")
