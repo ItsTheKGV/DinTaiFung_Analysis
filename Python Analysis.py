9ff92f62143ba393ac1b770b669e332a2e103bb1
ItsTheKGV <keanugene.valibia@gmail.com>
2024-06-03 01:30:10 -0500

    import libraries, merge dataframes, rename columns

--- Python Analysis.py
+++ Python Analysis.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib as mp
+import seaborn as sb
 
 ## import csv files as dataframes
 detail = pd.read_csv('Raw Data/detail.csv')
@@ -13,3 +16,9 @@ print(order.head(5))
 print(detail.dtypes)
 print(menu.dtypes)
 print(order.dtypes)
+
+detail = detail.rename(columns={"item_id":"product_id"})
+detailed_orders = detail.merge(menu, how = "inner", on = "product_id")
+detailed_orders = detailed_orders.drop(columns=["price_x"])
+detailed_orders = detailed_orders.rename(columns={"price_y":"price"})
+print(detailed_orders.head(10))
